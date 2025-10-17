544d0384ef8e232faad711f1079d30cdb496e5de
William Stein <thewillstein@gmail.com>
2025-10-16 20:33:09 -0400

    Initial commit

---
+++ src/main/java/com/example/hotelpricingproject/HotelPricingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.hotelpricingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelPricingProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelPricingProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hotelpricingproject/HotelPricingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelpricingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelPricingProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
